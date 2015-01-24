7f3dd031dbe2689787f36fa759d740016b973876
obrien1jj <obrien1jj@gmail.com>
2015-01-24 15:20:38 -0500

    some new scripts

---
+++ project/Assets/Scripts/OnDeath.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Do_Stuff(){
+		//possibly print some dialogue
+		//make hero run off into a random adjacent room
+			//prevent action mode from activating
+		//destroy the hero object
+		//possilby print some dialogue
+		//make a dead body in that room
+
+
+
+		}
+}
+
+/*
+ * matrix of references to tiles or something
+ */
+
