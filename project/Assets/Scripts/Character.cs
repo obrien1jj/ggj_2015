814c6b617176150966bee5100518caa1a786ba09
Aaron Horne <spam4aaron@gmail.com>
2015-01-24 14:53:51 -0500

    ActionMode Switch

--- project/Assets/Scripts/Character.cs
+++ project/Assets/Scripts/Character.cs
@@ -10,7 +10,25 @@ public class Character : MonoBehaviour
 	protected AnimatorStateInfo st;
 	protected AnimatorStateInfo nst;
 
+	public Material actionModeTintMaterial;
+
 	public CharacterController controller;
+
+	private bool _actionMode;
+	protected bool actionMode {
+		get { return _actionMode; }
+		set 
+		{
+			if (_actionMode != value)
+			{
+				Color temp = actionModeTintMaterial.color;
+				temp.a = value?0.4f:0;
+				actionModeTintMaterial.color = temp;
+				_actionMode = value;
+			}
+
+		}
+	}
 
 	virtual protected void Awake()
 	{
@@ -37,5 +55,9 @@ public class Character : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 	
+		actionMode = Input.GetKey(KeyCode.Space);
+
+
+
 	}
 }
