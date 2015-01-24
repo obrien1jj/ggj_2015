7cadfca4b031eb9e0e933d1959b669466700a219
Aaron Horne <spam4aaron@gmail.com>
2015-01-24 11:45:15 -0500

    RoomTemplate

--- project/Assets/Scripts/Character.cs
+++ project/Assets/Scripts/Character.cs
@@ -1,13 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour 
+{
+	protected Animator animator;
+	protected Transform thisTransform;
+	protected GameObject thisGameObject;
+	protected Transform controllerXfrm;
+	protected AnimatorStateInfo st;
+	protected AnimatorStateInfo nst;
+
+	public CharacterController controller;
+
+	virtual protected void Awake()
+	{
+		thisTransform = GetComponent<Transform>();
+		thisGameObject = gameObject;
+		
+		controller = GetComponentInChildren<CharacterController>();
+		controllerXfrm = controller.transform;
+
+		if (animator == null)
+			animator = controller.GetComponentInChildren<Animator>();
+	}
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	virtual protected void OnDestroy()
+	{
+
+	}
+		
 	// Update is called once per frame
 	void Update () {
 	
