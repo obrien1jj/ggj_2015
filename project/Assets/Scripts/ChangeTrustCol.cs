bc78f97a459569890d04d3fb0ed94acd48a6ce0c
Alex <agrundw@gmail.com>
2015-01-24 12:18:27 -0500

    Basic Trust value changing, Collisions not working

---
+++ project/Assets/Scripts/ChangeTrustCol.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTrustCol : MonoBehaviour {
+
+	public int dTrust = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision other) {
+
+		Debug.Log ("weeee");
+		if (other.gameObject.CompareTag ("Hero")) {
+			print("test");
+			other.gameObject.GetComponent<TrustValueTest>().ChangeTrust(dTrust);
+		}
+	}
+}
+
---
+++ project/Assets/Scripts/TrustValueTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrustValueTest : MonoBehaviour {
+
+	public int Trust = 20;
+	int startTrust;
+
+	// Use this for initialization
+	void Start () {
+		startTrust = Trust;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeTrust (int dTrustVal) {
+		return;
+		Trust += dTrustVal;
+	}
+}
