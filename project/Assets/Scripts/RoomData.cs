9722ab0821fcdd3e25bde7f2639da225bac8fb4b
obrien1jj <obrien1jj@gmail.com>
2015-01-24 22:27:04 -0500

    moar room stuff

--- project/Assets/Scripts/RoomData.cs
+++ project/Assets/Scripts/RoomData.cs
@@ -1,17 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RoomData : MonoBehaviour {
-
-
 	public enum Tile_Type {small_spike, big_spike, pusher, arrow_trap, lever, pit, holes, walls, key, gate};
-
-
 
 	public Tile_Type test = Tile_Type.small_spike;
 
 	//a matrix of the room's layout
-	Tile_Type[,] myArray = new Tile_Type[8,8];
+	Tile_Type[,] layout = new Tile_Type[8,8];
 
 	//what doors does this room have?  these are inside the wall so they aren't a tile.
 	bool door_top_right;
@@ -19,10 +16,20 @@ public class RoomData : MonoBehaviour {
 	bool door_bot_left;
 	bool door_top_left;
 
+	//a list of all the traps in the room
+	List<GameObject> trap_list = new List<GameObject>();
+
+
+
 	// Use this for initialization
 	void Start () {
-		myArray[0, 0] = test;
-		print (myArray[0,0]);
+		layout[0, 0] = test;
+		print (layout[0,0]);
+
+		foreach (Tile_Type element in layout) {
+			trap_list.Add(element);
+		}
+
 	}
 	
 	// Update is called once per frame
