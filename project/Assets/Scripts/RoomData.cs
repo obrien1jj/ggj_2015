c2ead88a101949e9daaf32f2cbb91ea10dc93ff8
Ameen Radwan <therespie@gmail.com>
2015-01-25 14:16:02 -0500

    Revert "Changed name of Prefabs to Resources and fixed Room Update"
    This reverts commit 3cfe9a4624cae4fd1efd1ea69a55860213014e95.

--- project/Assets/Scripts/RoomData.cs
+++ project/Assets/Scripts/RoomData.cs
@@ -39,58 +39,58 @@ public class RoomData : MonoBehaviour {
 				switch(ObjType)
 				{
 				case TileType.AxeX:
-					Instantiate(Resources.Load("AxeX"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(AxeX), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.AxeZ:
-					Instantiate(Resources.Load("AxeZ"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(AxeZ), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.ChestXp:
-					Instantiate(Resources.Load("ChestXp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(ChestXp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.ChestXn:
-					Instantiate(Resources.Load("ChestXn"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(ChestXn), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.ChestZp:
-					Instantiate(Resources.Load("ChestZp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(ChestZp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.ChestZn:
-					Instantiate(Resources.Load("ChestZn"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(ChestZn), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.Pillar:
-					Instantiate(Resources.Load("Pillar"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(Pillar), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.PistonXp:
-					Instantiate(Resources.Load("PistonXp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(PistonXp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.PistonXn:
-					Instantiate(Resources.Load("PistonXn"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(PistonXn), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.PistonZp:
-					Instantiate(Resources.Load("PistonZp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(PistonZp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.PistonZn:
-					Instantiate(Resources.Load("PistonZn"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(PistonZn), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.Spikes:
-					Instantiate(Resources.Load("Spikes"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(Spikes), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.SwitchXp:
-					Instantiate(Resources.Load("SwitchXp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(SwitchXp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.SwitchXn:
-					Instantiate(Resources.Load("SwitchXn"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(SwitchXn), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.SwitchZp:
-					Instantiate(Resources.Load("SwitchZp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(SwitchZp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.SwitchZn:
-					Instantiate(Resources.Load("SwitchZn"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(SwitchZn), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.SwitchYp:
-					Instantiate(Resources.Load("SwitchYp"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(SwitchYp), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.TheBigSpike:
-					Instantiate(Resources.Load("TheBigSpike"), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), Quaternion.identity);
+					Instantiate(Resources.Load(TheBigSpike), new Vector3((float)(i * 1.25 - 4.5), (float)(j * -1.25 + 4.5), 0), new Vector3(0, 0, 0));
 					break;
 				case TileType.NullObj:
 					break;
