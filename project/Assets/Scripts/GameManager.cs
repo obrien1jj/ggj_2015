8c6111f786fc93944cc21121cb3317dcaa80635a
Aaron Horne <spam4aaron@gmail.com>
2015-01-25 14:01:28 -0500

    Hero Skinning and UI Hero Stuff

--- project/Assets/Scripts/GameManager.cs
+++ project/Assets/Scripts/GameManager.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 
+	[System.Serializable]
+	public class HeroData
+	{
+		public string name;
+		public Material skin;
+		public int spriteID;
+		  
+	}
+
+	public tk2dSprite heroPortrait; 
+	public tk2dTextMesh heroNameText;
+	public HeroData currHeroData;
+	public SkinnedMeshRenderer heroMeshRenderer;
+
 	private static GameManager instance = null;
 	public static GameManager Instance {
 		get {
@@ -43,11 +57,29 @@ public class GameManager : MonoBehaviour {
 
 	[SerializeField] protected GameObject _wayPointMarker;
 	public GameObject wayPointMarker { get { return _wayPointMarker; }  }
+	public HeroData[] heroes = new HeroData[8];
+
+
+	void Start()
+	{
+		//pick a random hero
+		Random.seed = System.Environment.TickCount;
+		int selectedIndex = Mathf.FloorToInt(Random.value * 8f);
+		Debug.Log ("Selected: " + selectedIndex);
+
+		currHeroData = heroes[selectedIndex];
+
+		heroPortrait.SetSprite(currHeroData.spriteID);
+		heroNameText.text = currHeroData.name;
+		heroMeshRenderer.material = currHeroData.skin;
+
+	}
 
 	protected void Update () {
-		if (!disableActionMode)
-			actionMode = Input.GetKey(KeyCode.Space);
-		
+//		if (!disableActionMode)
+//			actionMode = Input.GetKey(KeyCode.Space);
+
+
 	}
 
 }
