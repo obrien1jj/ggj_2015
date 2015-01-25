a2e1153412ed04e141680a2cad17b24b761d7e92
obrien1jj <obrien1jj@gmail.com>
2015-01-25 09:45:33 -0500

    welcome to a world of sound!

--- project/Assets/Scripts/HeroCharacter.cs
+++ project/Assets/Scripts/HeroCharacter.cs
@@ -6,7 +6,8 @@ public class HeroCharacter : Character
 {
 	public class WayPoint
 	{
-		public WayPoint(Vector3 loc, GameObject mark = null, int num = 0)
+		public WayPoint(Vector3 loc, GameObject mark, int num)
+		//public WayPoint(Vector3 loc, GameObject mark = null, int num = 0)
 		{
 			pos = loc;
 			marker = mark;
--- project/Assets/Scripts/Personality.cs
+++ project/Assets/Scripts/Personality.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 //basically, this is how much trust they start with, how much trust they get from treasure, and how much they lose from traps, how much they lose fro hitting a bunch of traps
@@ -13,16 +14,35 @@ public class Personality : MonoBehaviour {
 	//numbers and stuff for the 3 things
 	//public enum Personality_Type {ambitious, cautious, rugged, feeble, forgiving, harsh, unquestioning, skeptical, greedy, pious};
 	//public Personality_Type attitude = Personality_Type.ambitious;
+
+
+	AudioClip MyAudio;
+
+	public List<AudioClip> cleared_room = new List<AudioClip>();
+	public List<AudioClip> hit_by_trap = new List<AudioClip>();
+	public List<AudioClip> opens_treasure_chest = new List<AudioClip>();
+	public List<AudioClip> run_out_of_trust = new List<AudioClip>();
+	public List<AudioClip> upon_selection = new List<AudioClip>();
+
+
 	// Use this for initialization
 	void Start () {
-		//gameObject.GetComponent <TrustValueTest>().Trust = 50;
+	
 	}
-	//blah blah blah
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
 	}
-	//eamg
+
+	void play_dialogue(List<AudioClip> g_audio_list){
+		AudioSource.PlayClipAtPoint(g_audio_list[(int)Random.Range (0.0F, g_audio_list.Count)], transform.position);
+	}     
+
+
+
+
 	//takes the stats and generates player-facing traits from their values
 	void generate_traits(){
 		
